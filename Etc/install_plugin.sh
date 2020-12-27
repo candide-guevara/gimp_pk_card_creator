7c1b2315bb7c48a89f5f9f4d5f4704684cbc46d7
candide guevara <candide-guevara@users.noreply.github.com>
2020-12-27 12:09:28 +0100

    Search for plugin dir across different versions of git

--- Etc/install_plugin.sh
+++ Etc/install_plugin.sh
@@ -6,59 +6,100 @@ USAGE="Installs pk render plugins and test runs them.
   -c    Clean the gimp plugins repo before installation
 "
 
-ROOT_DIR=`dirname $0`
-ROOT_DIR=`readlink -f $ROOT_DIR/..`
-PLUGIN_DIR=~/.gimp-2.8/plug-ins
-TEST_IMAGE="$ROOT_DIR/SourceImages/test_image.png"
-TEST_BATCH="$ROOT_DIR/Batches/batch_example.xml"
+REPO_ROOT=''
+PLUGIN_DIR=''
 
 run_cmd() {
   echo "Running : $@"
   $@
 }
 run_gimp() {
-  if [[ $RUN_WITH_GUI == 1 ]]; then
-    gimp_cmd=( gimp -b "( $* )" )
+  if [[ "$RUN_WITH_GUI" == 1 ]]; then
+    local -a gimp_cmd=( gimp -b "( $* )" )
   else
-    gimp_cmd=( gimp-console -b "( $* )" -b "(gimp-quit 0)" )
+    local -a gimp_cmd=(
+      gimp-console --no-interface --new-instance
+      -b "( $* )" -b "(gimp-quit 0)"
+    )
   fi
   echo "Running : ${gimp_cmd[@]}"
   "${gimp_cmd[@]}"
+}
+exit_with_msg() {
+  echo "[ERROR] $1"
+  exit 1
+}
+
+find_repo_root() {
+  local script_root="`readlink -f "$1/.."`"
+  local cwd="`pwd`"
+  local repo_from_script="`cd "$script_root" && git rev-parse --show-toplevel 2> /dev/null`"
+  local repo_from_cwd="`git rev-parse --show-toplevel 2> /dev/null`"
+
+  [[ -d "$repo_from_cwd" ]] && REPO_ROOT="$repo_from_script"
+  [[ -d "$repo_from_script" ]] && REPO_ROOT="$repo_from_script"
+  [[ -d "$REPO_ROOT" ]] || exit_with_msg "REPO_ROOT='$REPO_ROOT' is not a directory"
+}
+
+find_plugin_dir() {
+  # https://www.gimp.org/man/gimprc.html
+  [[ -f "$HOME/.gimprc" ]] && grep --quiet plug-in-path "$HOME/.gimprc" \
+    && exit_with_msg "'$HOME/.gimprc' plug-in-path not supported"
+  local version_sort_file="`mktemp`"
+  local candidate_file="`mktemp`"
+  {
+     find "$HOME" -maxdepth 1 -type d -iname '.gimp*'
+     [[ -z "$XDG_CONFIG_HOME" ]] || find "$XDG_CONFIG_HOME/GIMP" -maxdepth 1 -mindepth 1 -type d
+     find "$HOME/.config/GIMP" -maxdepth 1 -mindepth 1 -type d
+  } > "$candidate_file"
+  grep --quiet '' "$candidate_file" || exit_with_msg "No candidates for PLUGIN_DIR found"
+
+  gawk -e 'match($0, /.*([0-9]+)\.([0-9]+)$/, grp) 
+             { printf("%03d%03d %s\n", grp[1], grp[2], $0); }' "$candidate_file" \
+    | sort -nf \
+    | sed -r 's/^.* //' \
+    > "$version_sort_file"
+  local latest_dir="`cat "$version_sort_file" | tail -n1`"
+  PLUGIN_DIR="$latest_dir/plug-ins"
+
+  [[ -d "$PLUGIN_DIR" ]] || exit_with_msg "PLUGIN_DIR='$PLUGIN_DIR' is not a directory"
 }
 
 install_plugin() {
   [[ $TEST_ONLY == 1 ]] && return
 
-  pushd $PLUGIN_DIR
   # [CAUTION] Cleans everything
-  [[ $CLEAN_ALL == 1 ]] && run_cmd rm -rf $PLUGIN_DIR/*
-  run_cmd mkdir ./Batches
-  run_cmd cp -rf $ROOT_DIR/Batches/*.xml ./Batches
-  run_cmd cp -rf $ROOT_DIR/SourceImages .
-  run_cmd cp -r $ROOT_DIR/Python/* .
-  find -iname '*.pyc' | xargs rm
-  popd
+  [[ "$CLEAN_ALL" == 1 ]] \
+    && find "$PLUGIN_DIR" -mindepth 1 -maxdepth 1 | xargs --no-run-if-empty rm -rf
+
+  [[ -d "$PLUGIN_DIR"/Batches ]] || run_cmd mkdir "$PLUGIN_DIR"/Batches
+  run_cmd cp -rf $REPO_ROOT/Batches/*.xml "$PLUGIN_DIR"/Batches
+  run_cmd cp -rf $REPO_ROOT/SourceImages "$PLUGIN_DIR"
+  run_cmd cp -r $REPO_ROOT/Python/* "$PLUGIN_DIR"
+  find "$PLUGIN_DIR" -iname '*.pyc' | xargs --no-run-if-empty rm
 }
 
 run_plugins() {
-  [[ $INSTALL_ONLY == 1 ]] && return
+  [[ "$INSTALL_ONLY" == 1 ]] && return
+  local test_image="$REPO_ROOT/SourceImages/test_image.png"
+  local test_batch="$REPO_ROOT/Batches/batch_example.xml"
 
   energy_render=( python-fu-energy-card RUN-NONINTERACTIVE '"fire"' )
   run_gimp ${energy_render[@]}
 
   trainer_render=( python-fu-trainer-card RUN-NONINTERACTIVE 
-    '"test_trainer"' '"23"' '"'$TEST_IMAGE'"' '"Test trainer. Card description"' )
+    '"test_trainer"' '"23"' '"'$test_image'"' '"Test trainer. Card description"' )
   run_gimp ${trainer_render[@]}
 
   pk_render=( python-fu-pk-card RUN-NONINTERACTIVE
-    '"Test Pk"' '"PkName"' '"66"' '"leaf"' '"'$TEST_IMAGE'"'
+    '"Test Pk"' '"PkName"' '"66"' '"leaf"' '"'$test_image'"'
     '"attack"' 1 2 '"water"' '"test attack"' '"Attack description"' '"10+"'
     '"attack"' 1 3 '"fire"' '"test attack"' '"Second attack description"' '"30"'
     '"psy"' '"punch"' 2 '"pk description lv66"'
   )
   run_gimp ${pk_render[@]}
 
-  batch_render=( python-fu-batch-render RUN-NONINTERACTIVE '"'$TEST_BATCH'"' )
+  batch_render=( python-fu-batch-render RUN-NONINTERACTIVE '"'$test_batch'"' )
   run_gimp ${batch_render[@]}
 }
 
@@ -72,6 +113,8 @@ while getopts "gcith" arg; do
   esac
 done
 
+find_plugin_dir
+find_repo_root "$0"
 install_plugin
 run_plugins
 
--- Python/util/Cst_.py
+++ Python/util/Cst_.py
@@ -5,8 +5,36 @@ Created on May 16, 2009
 '''
 import os, logging, sys, re
 
+# Finds the higher version gimp plugin folder
+# Runs **before** logging is configured
+def findPluginDir():
+  version_rx = re.compile('(\d+)\.(\d+)$')
+  prjDir = ''
+  candidates = [ os.path.join(os.environ['HOME'], p) 
+                 for p in next(os.walk(os.environ['HOME']))[1]
+                 if os.path.basename(p).startswith('.gimp') ]
+  paths = [
+    os.path.join(os.environ['HOME'], '.config', 'GIMP'),
+    os.path.join(os.environ.get('XDG_CONFIG_HOME', ''), 'GIMP'),
+  ]
+  for path in paths:
+    if os.path.isdir(path):
+      candidates.extend(os.path.join(path, p) for p in next(os.walk(path))[1])
+
+  def sort_key(p):
+    m = version_rx.search(p)
+    return int(m.group(1)) * 100 + int(m.group(2))
+
+  prjDir = os.path.realpath(os.path.join(
+    sorted(candidates, key=sort_key)[-1],
+    'plug-ins'
+  ))
+  if not os.path.isdir(prjDir):
+    raise Exception("PRJ_DIR='%s' is not a directory" % prjDir)
+  return prjDir
+
 class Cst:
-  PRJ_DIR = os.path.expanduser("~/.gimp-2.8/plug-ins")
+  PRJ_DIR = findPluginDir()
   LOG_FILE = PRJ_DIR + '/poke_plugins.log'
   
   PK_IMG_DIR = PRJ_DIR + "/SourceImages/Pokemons"
@@ -14,7 +42,6 @@ class Cst:
   E_IMG_DIR = PRJ_DIR + "/SourceImages/Energies"
   E_TEMPLATE_DIR = PRJ_DIR + "/SourceImages/GimpWork/Energy"
   
-  BATCHJOBS_DIR = "Batches"
   DB_XML_FILE = PRJ_DIR + "/Batches/PkCardsClassicSet.xml"
   
   PLUGIN_FOLDER = "<Toolbox>/PkRender"
@@ -34,6 +61,11 @@ def findImageFile(dirname, name):
   assert os.path.isfile(fullPath)
   return fullPath
 
+def checkPluginsInstalled():
+  check_paths = [ Cst.PK_IMG_DIR, Cst.TR_IMG_DIR, Cst.E_IMG_DIR, Cst.E_TEMPLATE_DIR, ]
+  if not all( os.path.isdir(p) for p in check_paths ):
+    raise Exception('At least 1 of %r is missing' % check_paths)
+
 def initLog ():
   blueFnt = '\033[94m'
   resetFnt = '\033[0m'
@@ -41,4 +73,5 @@ def initLog ():
   logging.basicConfig(level=logging.DEBUG, format=format, filename=Cst.LOG_FILE, filemode='a')
 
 initLog()
+checkPluginsInstalled()
 
